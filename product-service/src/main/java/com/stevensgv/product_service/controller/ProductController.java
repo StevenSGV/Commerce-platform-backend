4aaa45bdf799345453cf0942d86099cd3229a46d
StevenGV <stivengomez1058@gmail.com>
2026-03-17 17:20:38 -0500

    feat: complete product CRUD endpoints

--- product-service/src/main/java/com/stevensgv/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/stevensgv/product_service/controller/ProductController.java
@@ -1,15 +1,41 @@
 package com.stevensgv.product_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stevensgv.product_service.model.Product;
+import com.stevensgv.product_service.service.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Product service is running...";
+    private final IProductService productService;
+
+    @GetMapping
+    public List<Product> getProductList() {
+        return productService.getProductList();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        productService.saveProduct(product);
+        return productService.getProductById(product.getId());
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id,
+                                 @RequestBody Product product) {
+        productService.updateProduct(id, product);
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
