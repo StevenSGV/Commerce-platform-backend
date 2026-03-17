0b11619b58b7d2d8d06388a685d6368850bd5f8c
StevenGV <stivengomez1058@gmail.com>
2026-03-17 16:10:37 -0500

    feat: initialize gateway service project

---
+++ gateway-service/src/test/java/com/stevensgv/gateway_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevensgv.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
