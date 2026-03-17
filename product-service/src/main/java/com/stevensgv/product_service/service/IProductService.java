5e212f0a8c0f53dd0d047344a28d8deaeeddbaee
StevenGV <stivengomez1058@gmail.com>
2026-03-17 17:10:21 -0500

    feat: implement basic product CRUD

---
+++ product-service/src/main/java/com/stevensgv/product_service/service/IProductService.java
@@ -0,0 +1,18 @@
+package com.stevensgv.product_service.service;
+
+import com.stevensgv.product_service.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+
+    List<Product> getProductList();
+
+    Product getProductById(Long id);
+
+    void saveProduct(Product product);
+
+    void updateProduct(Long id, Product product);
+
+    void deleteProduct(Long id);
+}
---
+++ product-service/src/main/java/com/stevensgv/product_service/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.stevensgv.product_service.service;
+
+import com.stevensgv.product_service.exception.NotFoundException;
+import com.stevensgv.product_service.model.Product;
+import com.stevensgv.product_service.repository.IProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService{
+
+    private final IProductRepository productRepository;
+
+    @Override
+    public List<Product> getProductList() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new NotFoundException("Product not found."));
+    }
+
+    @Override
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void updateProduct(Long id, Product product) {
+        Product findProduct = this.getProductById(id);
+
+        findProduct.setName(product.getName());
+        findProduct.setDescription(product.getDescription());
+        findProduct.setPrice(product.getPrice());
+
+        productRepository.save(findProduct);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
