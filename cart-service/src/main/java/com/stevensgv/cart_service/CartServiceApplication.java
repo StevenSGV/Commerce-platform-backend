bae5763cce57c0132a33b9985f528687452fa996
StevenGV <stivengomez1058@gmail.com>
2026-03-18 12:15:23 -0500

    feat: initialize and register cart service

---
+++ cart-service/src/main/java/com/stevensgv/cart_service/CartServiceApplication.java
@@ -0,0 +1,14 @@
+package com.stevensgv.cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class CartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplication.class, args);
+	}
+
+}
