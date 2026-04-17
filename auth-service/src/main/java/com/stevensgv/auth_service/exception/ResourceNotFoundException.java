76998545eea22571a053f21fabb8d558bbf2ab9f
StevenGV <stivengomez1058@gmail.com>
2026-04-17 13:36:57 -0500

    feat(exceptions): implement global exception handling package

---
+++ auth-service/src/main/java/com/stevensgv/auth_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.stevensgv.auth_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
