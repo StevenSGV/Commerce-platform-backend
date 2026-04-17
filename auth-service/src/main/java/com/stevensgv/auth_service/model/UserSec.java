0397abceb7ab4c996546a217e43e41e720a18f7b
StevenGV <stivengomez1058@gmail.com>
2026-04-17 12:09:19 -0500

    feat: create entities for auth-service

---
+++ auth-service/src/main/java/com/stevensgv/auth_service/model/UserSec.java
@@ -0,0 +1,47 @@
+package com.stevensgv.auth_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserSec {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 255)
+    @Column(nullable = false)
+    private String password;
+
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    @NotEmpty
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<Role>();
+}
