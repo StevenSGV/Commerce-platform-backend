0ae7d1676a591afc960918773f10a2ca76ae74c0
StevenGV <stivengomez1058@gmail.com>
2026-04-19 15:04:21 -0500

    feat: implement UserDetailsService

---
+++ auth-service/src/main/java/com/stevensgv/auth_service/service/CustomUserDetailsService.java
@@ -0,0 +1,48 @@
+package com.stevensgv.auth_service.service;
+
+import com.stevensgv.auth_service.model.UserSec;
+import com.stevensgv.auth_service.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserSec userSec = userRepository.findUserEntityByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+
+        userSec.getRoles()
+                .forEach(role ->
+                        authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getName()))));
+
+        userSec.getRoles().stream()
+                .flatMap(role ->
+                        role.getPermissions().stream())
+                .forEach(permission ->
+                        authorityList.add(new SimpleGrantedAuthority(permission.getName())));
+
+        return new User(
+                userSec.getEmail(),
+                userSec.getPassword(),
+                userSec.isEnabled(),
+                userSec.isAccountNonExpired(),
+                userSec.isAccountNonLocked(),
+                userSec.isCredentialsNonExpired(),
+                authorityList);
+    }
+}
