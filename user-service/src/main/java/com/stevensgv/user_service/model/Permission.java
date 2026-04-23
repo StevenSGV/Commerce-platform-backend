ba9e5c3b3d7f254166dc2405fe6b646be5c5f267
StevenGV <stivengomez1058@gmail.com>
2026-04-23 14:42:57 -0500

    refactor(entities): migrate profile data from auth-service to user-service

---
+++ user-service/src/main/java/com/stevensgv/user_service/model/Permission.java
@@ -0,0 +1,27 @@
+package com.stevensgv.user_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true, nullable = false)
+    private String name;
+}
